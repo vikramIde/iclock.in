7f7a2536a9336461fa9201baa93980c8f4818c27
vikram anand <vikrambanand.ide@gmail.com>
2015-11-20 16:52:45 +0530

    Changes of TargetController

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -96,7 +96,6 @@ class CollectionController extends Controller
 
          // $result = paymentrecieved::create($data);
 		  $this->saveComment($insertPayment['comment'],$insertPayment['date1'],$insertPayment['invoiceid']);
-
           $request->session()->flash('alert-success', 'Payment Has Been inserted Successfully');
           return redirect('collection/payment/'.$insertPayment['invoiceid'].'');
 
